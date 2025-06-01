9ddf2e517ef8777cbe64a568e2f7b05f4de0f99a
MatiasSF04 <matias.sf04@gmail.com>
2025-06-01 04:22:18 -0400

    Definir Consulta segun parametros externos

--- src/backend/Listado.java
+++ src/backend/Listado.java
@@ -18,6 +18,25 @@ import javax.swing.table.DefaultTableModel;
 public class Listado {
     public Listado(){}
     Principal Main;
+    Map<String, Integer> mapaAsignatura = Map.of(
+        "1. Matemáticas", 1,
+        "2. Programación", 2,
+        "3. Bases de Datos", 3,
+        "4. Sistemas Operativos y Redes", 4
+    );
+    Map<String, String> mapaTipo = Map.of(
+        "Seleccion Multiple", "selMultiple",
+        "Verdadero o Falso", "verOfal"
+    );
+    Map<String, Integer> mapaBloom = Map.of(
+        "1. Recordar", 1,
+        "2. Comprender", 2,
+        "3. Aplicar", 3,
+        "4. Analizar", 4,
+        "5. Evaluar", 5,
+        "6. Crear", 6
+    );
+
     
     public void MostrarLista(JTable tbLista){
         Conexion con = new Conexion();
@@ -30,8 +49,25 @@ public class Listado {
         modelo.addColumn("tiempo");
         modelo.addColumn("asignatura");
         
-        String Consulta = "SELECT * FROM Bloom.preguntas WHERE Tipo = 'selMultiple' && Nivel = '2' ORDER BY RAND() LIMIT 5;";
         try {
+            //Obtenemos las selecciones del usuario
+            String selecAsignatura = Main.cbAsignatura.getSelectedItem().toString();
+            String selecTipo = Main.cbTipo.getSelectedItem().toString();
+            String selecBloom = Main.cbBloom.getSelectedItem().toString();
+            String selecCantidad = Main.cbCantidad.getValue().toString();
+
+            //Las homologamos con los valores de nuestra base
+            int idAsignatura = mapaAsignatura.get(selecAsignatura);
+            String tipoSQL = mapaTipo.get(selecTipo);
+            int nivelBloom = mapaBloom.get(selecBloom);
+
+            //String Consulta = "SELECT * FROM Bloom.preguntas WHERE Tipo = 'selMultiple' && Nivel = '2' ORDER BY RAND() LIMIT 5;";
+            //Generamos la consulta
+            String Consulta = "SELECT * FROM Bloom.preguntas " +
+                              "WHERE Tipo = '" + tipoSQL + "' " +
+                              "AND Nivel = " + nivelBloom + " " +
+                              "AND Asignatura = " + idAsignatura + " " +
+                              "ORDER BY RAND() LIMIT " + selecCantidad;
             Statement st = cn.createStatement();
             ResultSet rs = st.executeQuery(Consulta);
             while(rs.next()) {
